cd29d459d5599598ed38f2ed55180eb0185cd83f
choquitofrito <zyriab@gmail.com>
2023-10-01 22:25:35 +0200

    end recherche filtres

--- ProjetFormulairesSymfony/src/Controller/FormRechercheLivresFiltresAjaxController.php
+++ ProjetFormulairesSymfony/src/Controller/FormRechercheLivresFiltresAjaxController.php
@@ -3,14 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Livre;
-use App\Form\SearchFiltreLivresType;
 use App\Form\RechercheiltreLivresType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 
@@ -20,19 +19,47 @@ class FormRechercheLivresFiltresAjaxController extends AbstractController
     public function rechercheLivresFiltres(Request $req, ManagerRegistry $doctrine, SerializerInterface $serializer): Response
     {
 
-        
+
         $form = $this->createForm(RechercheiltreLivresType::class);
-        
+
         $form->handleRequest($req);
-        
+
         if ($form->isSubmitted() && $form->isValid()) {
             $rep = $doctrine->getRepository(Livre::class);
             $resultats = $rep->rechercheLivresFiltres($form->getData());
 
-            $response = $serializer->serialize ($resultats, 'json', [AbstractNormalizer::IGNORED_ATTRIBUTES=> ['auteur']]) ;
-            // on renvoie le résultat JSON
-            return new Response ($response);
+            // Si on veut juste les livres, on serialise et on ignore les rélations pour éviter les références circulaires
+            // $response = $serializer->serialize ($resultats, 'json', [AbstractNormalizer::IGNORED_ATTRIBUTES=> ['exemplaires']]) ;
+            // Puis on renvoie le résultat JSON
+            // return new Response ($response);
 
+            // MAIS dans ce cas on a besoin aussi de l'auteur, alors on doit construire nous mêmes l'array ou définir
+            // un serialiser personnalisé. Ici on va le faire à la main (construire nous mêmes l'array)
+
+            $livresAvecNomsAuteurs = [];
+            foreach ($resultats as $livre) {
+                $arrLivre = [];
+                
+                $arrLivre['titre'] = $livre->getTitre();
+                $arrLivre['prix'] = $livre->getPrix();
+                // etc...
+                // créer la clé "nomsAuteur" 
+                $arrLivre['nomsAuteurs'] = [];
+                foreach ($livre->getAuteurs() as $auteur) {
+                    // rajouter le nom de l'auteur à l'array
+                    $arrLivre['nomsAuteurs'][] = $auteur->getNom();
+                }
+                // rajouter le livre ayant l'array d'auteurs incrusté
+                $livresAvecNomsAuteurs[] = $arrLivre;
+            }
+            
+            // on sérialise l'array qu'on vient de créer. On ignore les rélations, mais on 
+            // a incrusté l'array "nomsAuteurs". Sérialiser l'array ne posera aucun problème
+            // dd($livresAvecNomsAuteurs); // debuggez pour les voir...
+            $response = $serializer->serialize($livresAvecNomsAuteurs, 'json', [AbstractNormalizer::IGNORED_ATTRIBUTES => ['exemplaires','auteurs']]);
+            // dd ($response);    
+
+            return new Response ($response);
         }
 
         $vars = ['form' => $form];
--- ProjetFormulairesSymfony/src/Entity/Livre.php
+++ ProjetFormulairesSymfony/src/Entity/Livre.php
@@ -2,11 +2,12 @@
 
 namespace App\Entity;
 
-use App\Repository\LivreRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\LivreRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Serializer\Annotation\MaxDepth;
 
 #[ORM\Entity(repositoryClass: LivreRepository::class)]
 class Livre
